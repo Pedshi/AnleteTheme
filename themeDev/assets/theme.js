f72e952f408492f794f2b1a4ddedd36559c5f8ac
Pedram Shirmohammad <pedram.shir@hotmail.com>
2020-12-09 12:15:56 +0100

    new changes

--- themeDev/assets/theme.js
+++ themeDev/assets/theme.js
@@ -8337,13 +8337,13 @@ sections.register('skin-test-section', {
     switch(res){
       case 0:
         resHeader.textContent = 'Du har torr hud';
-        resText.textContent = 'En torr hud känns stram och kan även se grå och tråkig ut. Den är också mer benägen till att flagna och få huden att se "dammig" ut. Torr hud uppstår oftast p.g.a. att huden producerar mindre fett än vad som behövs och det är underskottet av fett leder till att huden har svårare för att hålla in vatten och därav blir uttorkad.';
-        resTextSol.textContent = 'Använd en ansiktskräm, helst en kräm som är lite tjockare och inte har består till större del av vatten, det hjälper till att hålla kvar vatten i huden. Vill du ge huden ordentligt med fukt och snabbare resultat så kan du använda en ansiktsmask som ger fukt. För bra effekt är det viktigt att rengöra ansiktet innan.';
+        resText.textContent = 'En torr hud känns stram och kan även se grå och tråkig ut. Den är också mer benägen till att flagna och få huden att se "dammig" ut. Torr hud uppstår oftast p.g.a. att huden producerar mindre fett än vad som behövs och det är detta underskottet av fett som leder till att huden har svårare för att hålla in vatten och därav blir uttorkad.';
+        resTextSol.textContent = 'Använd en ansiktskräm, helst en kräm som är lite tjockare, det hjälper till att hålla kvar vatten i huden. Vill du ge huden ordentligt med fukt och snabbare resultat så kan du använda en återfuktande ansiktsmask. För bra effekt är det viktigt att rengöra ansiktet innan.';
         break;
       case 1:
         resHeader.textContent = 'Du har fet hud';
         resText.textContent = 'En fet hud ser oftast blank och oljig ut, den är också mer benägen till att få finnar och pormaskar. Anledningen till att huden ser blank och oljig ut är på grund av hudens överproduktion av fett. Det är även denna överskott som kan täppa igen porer som sedan blir till pormaskar och finnar.';
-        resTextSol.textContent = 'Använd rengöringsprodukter som är exfolierande och skonsamma , utan parfym, för att få bort extra fett och smuts. Efter att du rengjort ansiktet är det viktigt att följa upp med en lättare ansiktskräm, annars kan huden producera mer fett än tidigare för att kompensera det som precis tvättats bort!';
+        resTextSol.textContent = 'Använd rengöringsprodukter som är exfolierande och skonsamma för att få bort extra fett och smuts. Efter att du rengjort ansiktet är det viktigt att följa upp med en lättare ansiktskräm, annars kan huden producera mer fett än tidigare för att kompensera det som precis tvättats bort!';
         break;
       case 2:
         resHeader.textContent = 'Du har normal hud';
@@ -8353,7 +8353,7 @@ sections.register('skin-test-section', {
       case 3:
         resHeader.textContent = 'Du har blandad hud';
         resText.textContent = 'En blandad hud eller kombinations hud är den vanligaste hudtypen, den är fet på T-zonen (pannan, näsan och hakan) men torr på kinderna och käkbenen.';
-        resTextSol.textContent = 'Har man blandad hud ska man använd milda och skonsamma produkter. Tänk på att inte använda för starka produkter på ställen du är torr. Använd en djuprengörande ansiktsmask på de fetare platser och en återfuktande ansiktsmask på de torra platser. ';
+        resTextSol.textContent = 'Använd milda och skonsamma produkter och tänk på att inte använda för starka produkter på ställen du är torr. Punktbehandla olika delar av ansiktet genom att använda en djuprengörande ansiktsmask på de fetare platserna och en återfuktande ansiktsmask på de torra platserna.';
         break;
     }
 
